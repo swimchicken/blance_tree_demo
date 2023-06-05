93c0400337d88c20ae9a72a735c0f61085a4e3fb
swimchicken <swimchickenOuO@gmail.com>
2023-06-05 16:56:30 +0800

    update1

--- query_main.py
+++ query_main.py
@@ -1,58 +1,26 @@
 import csv
 
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-
 class BalancedBinaryTree:
     def __init__(self):
-        self.root = None
+        self.data_dict = {}
 
     def insert(self, data):
-        if self.root is None:
-            self.root = Node(data)
-
-        else:
-            current = self.root
-            while 1:
-                # ----------
-                if data[0] < current.data[0]:
-                    if current.left is None:
-                        current.left = Node(data)
-                        break
-                    else:
-                        current = current.left
-
-                # ----------
-                else:
-                    if current.right is None:
-                        current.right = Node(data)
-                        break
-                    else:
-                        current = current.right
+        student_id = data[0]
+        if student_id not in self.data_dict:
+            self.data_dict[student_id] = []
+        self.data_dict[student_id].append(data[2])  # 存儲課程名稱
 
     def search(self, key):
-
-        result_serch = []
-        current = self.root
-        while current is not None:
-            if current.data[0] == key:
-                result_serch.append(current.data[2])
-            elif key < current.data[0]:
-                current = current.left
-            else:
-                current = current.right
-
-        return result_serch
+        if key in self.data_dict:
+            return self.data_dict[key]
+        else:
+            return None
 
 
 tree = BalancedBinaryTree()
 
-for i in range(0, 4653):
+for i in range(0, 4654):
     file_name = f"reset_data_total/reset_data_total_{i}.csv"
     with open(file_name, "r", encoding="utf-8") as file:
         reader = csv.reader(file)
